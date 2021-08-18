18b7f5af6d29428cebcc876df6ea5d200238c931
Carla <ccairns1999@gmail.com>
2021-08-18 13:40:55 -0500

    tab between panels

--- src/newItemPanel.js
+++ src/newItemPanel.js
@@ -1,3 +1,5 @@
+import {getDivChildren} from "./website";
+
 let loadNewItemPanel = () =>{
     const itemPanel = document.querySelector(".overlay");
     itemPanel.classList.remove("display-none");
@@ -8,4 +10,23 @@ let closeNewItemPanel = () =>{
     itemPanel.classList.add("display-none");
 }
 
-export {loadNewItemPanel,closeNewItemPanel };
+//display none for other tabs
+let hideInactiveTabs = (activePanel) =>{
+    let tabsArr = getDivChildren("new-item-main-content", 'item-panel');
+    tabsArr.forEach(element => {
+        if(activePanel.classList[0] === element.classList[0]){
+            activePanel.classList.remove("display-none");
+        }
+        else{
+            element.classList.add("display-none");
+        }
+    });
+}
+// loading the different panels
+let displayTab = (activePanel) =>{
+    hideInactiveTabs(activePanel);
+
+}
+
+
+export {loadNewItemPanel,closeNewItemPanel,displayTab };
--- src/website.js
+++ src/website.js
@@ -1,4 +1,4 @@
-import {loadNewItemPanel, closeNewItemPanel } from "./newItemPanel";
+import {loadNewItemPanel, closeNewItemPanel,displayTab } from "./newItemPanel";
 
 let initEventListeners = () => {
     const newTodoBtn = document.querySelector(".new-todo_btn");
@@ -10,6 +10,39 @@ let initEventListeners = () => {
     closePanel.addEventListener("click", (e) => {
         closeNewItemPanel();
     })
+
+    // new panel tabs
+    const dateTab = document.querySelector(".new-item-date");
+    const todoTab = document.querySelector(".new-item-todo");
+    const projectTab = document.querySelector(".new-item-project");
+    const noteTab = document.querySelector(".new-item-note");
+
+    todoTab.addEventListener("click", (e) => {
+        displayTab(document.querySelector(".todo-panel"));
+    })
+    projectTab.addEventListener("click", (e) => {
+        displayTab(document.querySelector(".project-panel"));
+    })
+    dateTab.addEventListener("click", (e) => {
+        displayTab(document.querySelector(".date-panel"));
+    })
+    noteTab.addEventListener("click", (e) => {
+        displayTab(document.querySelector(".note-panel"));
+    })
 }
 
-export {initEventListeners};
+let getDivChildren = (containerId, elementsId) =>{
+    var div = document.getElementById(containerId),
+        subDiv = div.getElementsByTagName('div'),
+        myArray = [];
+
+    for(let i = 0; i < subDiv.length; i++) {
+        let elem = subDiv[i];
+        if(elem.id == elementsId) {
+            myArray.push(elem);
+        }
+    }
+    return myArray;
+}
+
+export {initEventListeners, getDivChildren};
