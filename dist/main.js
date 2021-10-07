bf1d16ce3578a99652ecd5c7fb189582a1aa7c56
Carla <ccairns1999@gmail.com>
2021-10-07 14:52:43 -0500

    todo items can be edited and deleted with local storage

--- dist/main.js
+++ dist/main.js
@@ -662,7 +662,7 @@ let newTodoSubmition = (todos, newTodoSubmitBtn,newTodoTitle,newTodoDetails,newT
     })
 }
 
-let todoItemsArrayAndListeners = (arrayTodoItems) => {
+let todoItemsArrayAndListeners = (arrayTodoItems, todos) => {
     arrayTodoItems = (0,_websiteInit__WEBPACK_IMPORTED_MODULE_0__.getDivChildrenByClass)("items-container","todo-item_page");
     console.log(arrayTodoItems);
 
@@ -703,6 +703,8 @@ let todoItemsArrayAndListeners = (arrayTodoItems) => {
             else{
                 editBtn.classList.add("todo-edit_clicked");
                 displayTodoEditPanel(editBtn);
+                editTodoItem(editBtn, e, todos)
+
             }
         })
 
@@ -717,7 +719,9 @@ let todoItemsArrayAndListeners = (arrayTodoItems) => {
         })
 
         // deleting the todo items on click
-        deleteBtn.addEventListener("click", _websiteInit__WEBPACK_IMPORTED_MODULE_0__.deleteItem , (e) => {
+        deleteBtn.addEventListener("click", (e) => {
+            deleteTodoItem(deleteBtn, e, todos)
+
         })
 
         
@@ -762,6 +766,105 @@ let loadTodos = (todos,itemsContainer, clicked) => {
 
     })
 
+}
+let editTodoPriority = () =>{
+    let priorityContainer = document.querySelector(".edit-todo-priority");
+    let priorityArr= Array.from(document.querySelectorAll(".te-priority-btn"))
+    priorityContainer.addEventListener("click", (e)=>{
+        
+        //removes previously clicked btns and adds class to clicked btn
+        priorityArr.forEach(btn =>{
+            if(btn.id !== e.target.id){
+                btn.classList.remove("te-priority_clicked");
+                btn.classList.remove("te-priority-btn-"+btn.textContent.toLowerCase()+"_clicked")
+                btn.classList.add("te-priority-btn-"+btn.textContent.toLowerCase())
+            }
+            else {
+                btn.classList.add("te-priority_clicked");
+                btn.classList.add("te-priority-btn-"+btn.textContent.toLowerCase()+"_clicked")
+                btn.classList.remove("te-priority-btn-"+btn.textContent.toLowerCase())
+
+            }
+        });
+            
+       
+    })
+}
+let editTodoItem = (editBtn, e, todos) =>{
+    let editTodoTitle_panel = document.querySelector(".edit-todo-title");
+    let editTodoDetails_panel = document.querySelector(".edit-todo-details");
+    let editTodoPriority_panel = document.querySelector(".edit-todo-priority");
+    let editTodoDate_panel = document.querySelector("#edit-date_todo");
+
+    editTodoPriority_panel.addEventListener("click", (e) =>{
+        editTodoPriority();
+    })
+    let priorityArr= Array.from(document.querySelectorAll(".te-priority-btn"));
+    
+    //reset values
+    editTodoTitle_panel.value = "";
+    editTodoDetails_panel.value= "";
+    editTodoDate_panel.value="";
+    priorityArr.forEach(btn =>{
+        btn.classList.remove("te-priority_clicked");
+        btn.classList.remove("te-priority-btn-"+btn.textContent.toLowerCase()+"_clicked");
+        btn.classList.add("te-priority-btn-"+btn.textContent.toLowerCase());
+    })
+
+    let todoEditSumbitBtn = document.querySelector(".todo-edit-submit");
+    let todoToEdit = null;
+
+    let todoItem = e.target.parentElement.parentElement;
+    let todoPriority = todoItem.dataset.priority;
+    let todoTitleElement = todoItem.querySelector(".todo-title");
+    let todoDetailsElement = todoItem.querySelector(".todo-detail");
+    let todoDateElement = todoItem.querySelector(".todo-date")
+
+    let todoTitle = todoTitleElement.textContent.split('  ').join('');
+    let todoDetail = todoDetailsElement.textContent.split("item details").join("").split("\n").join("").split("  ").join("");
+    let todoDate = todoDateElement.textContent.split(' ').join('');
+    
+    //setting project edit starting panel values
+    editTodoTitle_panel.value = todoTitle;
+    editTodoDetails_panel.value = todoDetail;
+    editTodoDate_panel.value=todoDate;
+    priorityArr.forEach(btn =>{
+        if (btn.textContent.toLowerCase() == todoPriority){
+            btn.classList.add("te-priority_clicked");
+            btn.classList.add("te-priority-btn-"+btn.textContent.toLowerCase()+"_clicked")
+            btn.classList.remove("te-priority-btn-"+btn.textContent.toLowerCase())
+        }
+    })
+   
+    todoEditSumbitBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        //finding the corret date and changing the values on local storage
+        todoToEdit = todos.find((todo) => todo._id === editBtn.dataset.editTodo);
+        todoToEdit.title = editTodoTitle_panel.value;
+        todoToEdit.details = editTodoDetails_panel.value;
+        todoToEdit.dueDate = editTodoDate_panel.value;
+        priorityArr= Array.from(document.querySelectorAll(".te-priority-btn"));
+        priorityArr.forEach(btn =>{
+            if (btn.classList.contains("te-priority_clicked")){
+                todoToEdit.priority = btn.textContent.toLowerCase();
+
+            }
+        })
+
+
+        todoEditAutoClose();
+        (0,_websiteInit__WEBPACK_IMPORTED_MODULE_0__.saveAndLoad)();
+    });
+}
+let deleteTodoItem = (deleteBtn, e, todos) =>{
+    let todoToDeleteIndex = todos.findIndex((todo) => todo._id === deleteBtn.dataset.deleteTodo);
+    todos.splice(todoToDeleteIndex, 1);
+
+    (0,_websiteInit__WEBPACK_IMPORTED_MODULE_0__.saveAndLoad)();
+}
+let todoEditAutoClose = () =>{
+    let todoEditPanel = document.querySelector(".todo-item-edit");
+    todoEditPanel.classList.add("display-none");
 }
 
 
